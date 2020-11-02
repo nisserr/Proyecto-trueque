f7bed4dd2fc368d78d01c74f018d88f6b2360291
nisserr <nisserargueta@gmail.com>
2020-11-02 14:47:14 -0300

    Se arreglo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PersonalComponent } from './personal/personal.component';
-import { ClimaComponent } from './clima/clima.component';
-import { EmpleadosComponent } from './empleados/empleados.component';
+
+
 
 const routes: Routes = [
   {
-    path: '',
-    component: PersonalComponent
+   
   },
-  {
-    path: 'clima',
-    component: ClimaComponent
-  },
-  {
-    path: 'empleados',
-    component: EmpleadosComponent
-  }
+  
+ 
 ];
 
 @NgModule({
