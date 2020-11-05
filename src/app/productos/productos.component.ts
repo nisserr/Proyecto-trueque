048eff6b2d1acb30c4770d0146d82b7a3c879b5a
nisserr <nisserargueta@gmail.com>
2020-11-05 12:58:54 -0300

    Se agregó productos y bienvenida

--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -4,6 +4,8 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-productos',
   templateUrl: './productos.component.html',
   styleUrls: ['./productos.component.css']
+
+  
 })
 export class ProductosComponent implements OnInit {
 
